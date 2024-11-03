0236572effbfabdcd9295766f806f08780bf7f45
cqm3ron <camerondbissell@gmail.com>
2024-11-03 21:36:41 +0000

    Initial commit
    Added first few files. Will continue to add to this over the coming weeks / months / years (however long it takes me to figure out how tf to code the goddamn ghost logic)

---
+++ Program.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim grid() As Integer = {27, 30} 'i counted this manually
+    End Sub
+    Function GenerateGrid()
+        'im gonna kms if i decide to make the grids random
+
+    End Function
+End Module
